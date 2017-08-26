621dad063108a6095d6492fe226bc9804455fca7
wgra538 <wgra538@aucklanduni.ac.nz>
2017-08-26 15:01:55 +1200

    sliders for add-review page kinda working

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -2,11 +2,13 @@ import {MdInputModule} from '@angular/material';
 import {NgModule } from '@angular/core';
 import {MdSidenavModule} from '@angular/material';
 import {MdToolbarModule} from '@angular/material';
+import {MdSliderModule} from '@angular/material';
 
 const IMPORTS = [
   MdInputModule,
   MdSidenavModule,
-  MdToolbarModule
+  MdToolbarModule,
+  MdSliderModule,
 ]
 
 @NgModule({
