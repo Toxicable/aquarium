c7a3d5979c89996a5f3582de86c88c75ec0fc81b
Fabian Wiles <fabianwiles@live.com>
2017-08-26 15:42:06 +1200

    add company search

---
+++ src/app/models/company.ts
@@ -0,0 +1,10 @@
+export interface Company{
+    Company: string;
+    Logo: string;
+    Banner: string;
+    Cassifieds: Cassified[]
+}
+export interface Cassified{
+    Title: string;
+    ListingId: string;
+}
