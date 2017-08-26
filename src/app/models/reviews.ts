08c407e98244f228a50f6c2c012d4ec91152b24d
wgra538 <wgra538@aucklanduni.ac.nz>
2017-08-26 12:45:01 +1200

    added short description field in database field

--- src/app/models/reviews.ts
+++ src/app/models/reviews.ts
@@ -3,14 +3,19 @@ export interface Review{
     location: string;
     salary: number; // will point to a salary range which we can define in the UI
     allowances: string;
+    allowancesShort: string;
     allowancesRating: number;
     careerDev: string;
+    careerDevShort: string;
     careerDevRating: number;
     dressCode: string;
+    dressCodeShort: string;
     dressCodeRating: number;
     diversity: string;
+    diversityShort: string;
     diversityRating: number;
     CompStruct: string;
+    CompStructShort: string;
     CompStructRating: number;
 
 }
