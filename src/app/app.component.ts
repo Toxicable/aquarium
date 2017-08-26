fc4ecebc340bdf7bd1e333102cb3879949649d96
Fabian Wiles <fabianwiles@live.com>
2017-08-26 14:27:22 +1200

    header works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from "angularfire2/auth";
 import { AuthService } from "./auth.service";
-
-
+import * as firebase from 'firebase';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -15,5 +15,13 @@ export class AppComponent {
     public afAuth: AngularFireAuth,
   ){
     
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 }
