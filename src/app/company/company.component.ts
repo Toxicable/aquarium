34ccd1c7e3f9d966893c4f85fa0f3758b473f4b0
Fabian Wiles <fabianwiles@live.com>
2017-08-27 10:20:32 +1200

    a

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -22,23 +22,23 @@ export class CompanyComponent implements OnInit {
     constructor(
       private sanitiser: DomSanitizer,
       private route: ActivatedRoute,
-      private companiesService: CompanyService, 
+      private companiesService: CompanyService,
     ) {}
 
     company$: Observable<Company>;
 
     ngOnInit(){
-      this.company$ = this.route.paramMap.mergeMap(params => {
-        console.log(params);
+      const companyname$ = this.route.paramMap.map(params => params.get('companyName'))
+      companyname$.mergeMap(companyName => {
         return this.companiesService.getAll()
-          .map(companies =>{ 
-            const company = companies.find(c => c.Company === params.get('companyName'));
+          .map(companies =>{
+            const company = companies.find(c => c.Company === companyName);
             company.Banner = this.sanitiser.bypassSecurityTrustResourceUrl(company.Banner) as any;
             company.Logo = this.sanitiser.bypassSecurityTrustResourceUrl(company.Logo) as any;
             return company;
           })
       })
-      const tiles = [{"colour":"black"},{"colour":"red"}];
+
 
 
     }
