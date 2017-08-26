735a9a28b668d58dd9bfeae2c42d9a26f0173870
Fabian Wiles <fabianwiles@live.com>
2017-08-26 14:51:23 +1200

    companies json

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class CompanyService{
+
+    constructor(
+        private http: HttpClient
+    ){
+        this.http.get('assets/companies.json')
+    }
 
 }
