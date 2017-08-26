c7c06748f5c667526f80c49e627dcbf86a5f2660
KieranSScott <kewanscott@gmail.com>
2017-08-26 13:30:27 +1200

    added skeleton for dummy data

--- src/app/review/reviews.service.ts
+++ src/app/review/reviews.service.ts
@@ -3,20 +3,60 @@ import { Observable } from 'rxjs/Observable'
 import { Review } from "../models/reviews";
 
 @Injectable()
-export class ReviewsService{
+export class ReviewsService {
 
     reviews$: Observable<Review[]>;
 
     constructor(
 
-    ) { 
-         this.reviews$ = Observable.of([
-            {location: 'I am a title'}
-         ])
+    ) {
+        this.reviews$ = Observable.of([
+            {   $key: '' ,
+                companyName: '' ,
+                location: '' , 
+                salary: 0 ,
+                allowances: '' , 
+                allowancesShort: '' ,
+                allowancesRating: 0 ,
+                careerDev: '' ,
+                careerDevShort: '' ,
+                careerDevRating: 0 ,
+                dressCode: '' ,
+                dressCodeShort: '' ,
+                dressCodeRating: 0 ,
+                diversity: '' ,
+                diversityShort: '' ,
+                diversityRating: 0 ,
+                CompStruct: '' ,
+                CompStructShort: '' ,
+                CompStructRating: 0
+            } ,
+            {   $key: '231412342143' ,
+            companyName: 'google' ,
+            location: 'auckland' , 
+            salary: 4 ,
+            allowances: '' , 
+            allowancesShort: '' ,
+            allowancesRating: 0 ,
+            careerDev: '' ,
+            careerDevShort: '' ,
+            careerDevRating: 0 ,
+            dressCode: '' ,
+            dressCodeShort: '' ,
+            dressCodeRating: 0 ,
+            diversity: '' ,
+            diversityShort: '' ,
+            diversityRating: 0 ,
+            CompStruct: '' ,
+            CompStructShort: '' ,
+            CompStructRating: 0
+        }
+        ])
 
     }
 
-    searchByTitle(term: string){
-        return this.reviews$.map(r => r.filter(item => item.location.includes(term) ));
+    searchByTitle(term: string) {
+        return this.reviews$.map(r => r.filter(item => item.location.includes(term)));
     }
+
 }
